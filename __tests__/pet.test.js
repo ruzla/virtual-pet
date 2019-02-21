8be47519cbe53462bb845b3b7ca7f8a45a55020a
Russell <russelldoe@hotmail.co.uk>
2019-02-21 20:37:24 +0000

    completed naming pet

---
+++ __tests__/pet.test.js
@@ -0,0 +1,24 @@
+const Pet = require('../src/pet');
+
+describe('constructor', () => {
+
+    it('returns an object', () => {
+  
+      expect(new Pet('Fido')).toBeInstanceOf(Object);
+  
+    });
+  
+  });
+
+  describe('constructor', () => {
+  
+    it('sets the name property', () => {
+  
+      const pet = new Pet('Fido');
+  
+      expect(pet.name).toEqual('Fido');
+  
+    });
+  
+  });
+   
